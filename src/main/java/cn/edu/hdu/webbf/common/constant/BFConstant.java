5ff528ca1ea116171c6f635df016d5f5d040a6e5
xzz646448657 <xiezhengzhu@windows10.microdone.cn>
2019-01-23 16:39:29 +0800

    test

---
+++ src/main/java/cn/edu/hdu/webbf/common/constant/BFConstant.java
@@ -0,0 +1,25 @@
+/*
+ * File Name: BFConstant.java
+
+ * Description:
+ * Author: Pi Chen
+ * Create Date: 2016年9月3日
+
+ */
+package cn.edu.hdu.webbf.common.constant;
+
+/**
+ *
+ *
+ * @author Pi Chen
+ * @version webbf V1.0.0, 2016年9月3日
+ * @see
+ * @since webbf V1.0.0
+ */
+
+public class BFConstant
+{
+
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+}
